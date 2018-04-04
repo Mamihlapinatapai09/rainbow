9c893dc37a8834d6d446c7baceae0897f814f390
zhaoweiyun <zhaoweiyun@qiyeyoudao.com>
2018-04-04 17:20:56 +0800

    志愿者模块

--- client/module/admin/components/team/Index.vue
+++ client/module/admin/components/team/Index.vue
@@ -12,14 +12,14 @@
 
 				<!-- table列表 -->
 				<div class="table-box">
-					<el-table :data="teamList">
+					<el-table :data="teamList" @row-click="handlerVolunteer">
 						<el-table-column prop="name" label="团队名称"></el-table-column>
 						<el-table-column prop="leader" label="团队leader"></el-table-column>
 						<el-table-column prop="addtime" label="成立时间"></el-table-column>
 						<el-table-column v-if="activeTabName === '1'" label="操作" align="center">
 							<template slot-scope="scope">
-								<span class="operate" @click="handlerEdit(scope)">编辑</span>
-								<span class="operate" @click="handlerDelete(scope)">删除</span>
+								<span class="operate" @click="handlerEdit.stop(scope)">编辑</span>
+								<span class="operate" @click="handlerDelete.stop(scope)">删除</span>
 							</template>
 						</el-table-column>
 					</el-table>
@@ -35,13 +35,38 @@
 				</el-pagination>
 			</div>
 		</container-component>
-
+	
+		<!-- 删除弹窗 -->
 		<el-dialog title="删除"
 			:visible.sync="deleteDialogVisible">
 			<div>团队删除将无法恢复，是否删除该志愿者团队？</div>
 			<span slot="footer" class="dialog-footer">
 				<el-button @click="ajaxDelete">确 定</el-button>
 			    <el-button class="cancel" @click="deleteDialogVisible = false">取 消</el-button>
+			</span>
+		</el-dialog>
+
+		<!-- 团队志愿者弹窗 -->
+		<el-dialog :title="activeTeam.name+'的志愿者'"
+			:visible.sync="volunteerDialogVisible">
+			<div>
+				<el-table :data="volunteerList">
+					<el-table-column prop="name" label="姓名"></el-table-column>
+					<el-table-column prop="mobile" label="手机号"></el-table-column>
+				</el-table>
+
+				<!-- 分页 -->
+				<el-pagination
+				    layout="prev, pager, next"
+				    :page-size="6"
+				    :total="volunteerLen"
+				    @current-change="handlerVolunteerPage"
+				    :current-page="volunteerCurrentPage">
+				</el-pagination>
+			</div>
+
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="volunteerDialogVisible = false">确 定</el-button>
 			</span>
 		</el-dialog>
 	</div>
@@ -75,7 +100,25 @@ export default{
 					"images":"",  //团队照片
 					"status":1    // 0 已删除 1已添加
 				}
-			]
+			],
+			//---------- 志愿者列表 ------
+			volunteerDialogVisible:false,
+			volunteerLen:10, 
+			volunteerCurrentPage:1,
+			volunteerPageParam:{
+				page:1,
+				num:4,
+				teamId:''
+			},
+			volunteerList:[{
+			    "id": 0,
+			    "name": "大锤",
+			    "mobile": "13260119939",
+			    "email": "373345@qq.com",
+			    "idcode": "3248728",
+			    "sex": 0,
+			    "status": 0
+			}]
 		}
 	},
 	methods:{
@@ -122,6 +165,25 @@ export default{
 				}
 			})
 		},
+		// 获取志愿者列表
+		ajaxGetVolunteer(){
+			const t = this;
+			t.volunteerPageParam['teamId'] = t.activeTeam['id'];
+			t.$http({
+				method:'post',
+				url:'/team/ajax-get-team-volunteers',
+				body:t.volunteerPageParam
+			}).then(res => {
+				const result = res.data;
+				if(!result.status){
+					return t.$message({
+						message:result.message,
+						type:'error'
+					})
+				}
+				t.volunteerList = result.data.list;
+			})
+		},
 		//----------  页面操作 ------------
 		// 换页
 		handlerPage(page){
@@ -152,6 +214,19 @@ export default{
 		handlerAdd(){
 			const t = this;
 			t.$router.push('/team-list?teamId=');	
+		},
+		// 志愿者
+		handlerVolunteer(row, event, column){
+			const t = this;
+			t.activeTeam = row;
+			t.volunteerDialogVisible = true;
+			t.ajaxGetVolunteer();
+		},
+		// 志愿者换页
+		handlerVolunteerPage(page){
+			const t = this;
+			t.volunteerPageParam['page'] = page;
+			t.ajaxGetVolunteer();
 		}
 	},
 	components:{
@@ -179,19 +254,19 @@ export default{
 
 		.table-box{
 			height:83%;
+			cursor: pointer;
 			.operate{
 				padding:10px;
-				cursor: pointer;
 				&:hover{
 					color:$btnActive;
 				}
 			}
 		}
+	}
 
-		.el-pagination{
-			text-align: right;
-			margin-top: 15px;
-		}
+	.el-pagination{
+		text-align: right;
+		margin-top: 15px;
 	}
 }
 
--- client/module/admin/components/volunteer/Index.vue
+++ client/module/admin/components/volunteer/Index.vue
@@ -1,16 +1,221 @@
 <template>
-	<div class="team-container" style="height:100%">
+	<div class="volunteer-container" style="height:100%">
 		<container-component :activeSideBar="activeSideBar">
-			<div>volunteer</div>
+			<div class="volunteer-content">
+				<!-- tabs切换 -->
+				<el-tabs v-model="activeTabName" @click="handlerTab">
+					<el-tab-pane label="已注册" name="1"></el-tab-pane>
+					<el-tab-pane label="已注销" name="0"></el-tab-pane>
+				</el-tabs>
+
+				<!-- table列表 -->
+				<div class="table-box">
+					<el-table :data="volunteerList" @row-click="handlerActivity">
+						<el-table-column prop="name" label="姓名"></el-table-column>
+						<el-table-column prop="mobile" label="手机号"></el-table-column>
+						<el-table-column prop="time" label="注册时间"></el-table-column>
+						<el-table-column v-if="activeTabName === '1'" label="操作" align="center">
+							<template slot-scope="scope">
+								<span class="operate" @click.stop="handlerDelete(scope)">注销</span>
+							</template>
+						</el-table-column>
+					</el-table>
+				</div>
+
+				<!-- 分页 -->
+				<el-pagination
+				    layout="prev, pager, next"
+				    :page-size="6"
+				    :total="volunteerLen"
+				    @current-change="handlerPage"
+				    :current-page="currentPage">
+				</el-pagination>
+			</div>
 		</container-component>
+
+		<!-- 注销弹窗 -->
+		<el-dialog title="注销"
+			:visible.sync="deleteDialogVisible">
+			<div>志愿者注销将无法恢复，是否注销该志愿者？</div>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="ajaxDelete">确 定</el-button>
+			    <el-button class="cancel" @click="deleteDialogVisible = false">取 消</el-button>
+			</span>
+		</el-dialog>
+
+		<!-- 志愿者活动弹窗 -->
+		<el-dialog :title="activeVolunteer.name+'的活动'"
+			:visible.sync="activityDialogVisible">
+			<div>
+				<el-table :data="activityList">
+					<el-table-column prop="name" label="活动"></el-table-column>
+					<el-table-column prop="teamName" label="团队"></el-table-column>
+					<el-table-column prop="startDate" label="开始时间"></el-table-column>
+					<el-table-column prop="endDate" label="结束时间"></el-table-column>
+				</el-table>
+
+				<!-- 分页 -->
+				<el-pagination
+				    layout="prev, pager, next"
+				    :page-size="6"
+				    :total="activityLen"
+				    @current-change="handlerActivityPage"
+				    :current-page="activityCurrentPage">
+				</el-pagination>
+			</div>
+			<span slot="footer" class="dialog-footer">
+				<el-button @click="activityDialogVisible = false">确 定</el-button>
+			</span>
+		</el-dialog>
 	</div>
 </template>
 <script>
 import ContainerComponent from '../layout/Container.vue';
 export default{
+	created(){
+		const t = this;
+	},
 	data(){
 		return{
-			activeSideBar:"volunteer"
+			// -------  志愿者部分 --------
+			activeSideBar:"volunteer",
+			activeTabName:"1",
+			currentPage:1,
+			pageParam:{
+				page:1,
+				num:6,
+				type:1  // 0 已删除 1已添加
+			},
+			volunteerLen:10,
+			volunteerList:[{
+				"id":0,
+				"name":"张三", // 姓名
+				"mobile":"13166966115", // 手机号
+				"time":"2017-10-25", // 注册时间
+				"status":1    // 0 已注册 1已注销
+			}],
+			deleteDialogVisible:false,
+			activeVolunteer:'',
+			// --------- 活动列表 ----------
+			activityLen:10, 
+			activityCurrentPage:1,
+			activityPageParam:{
+				page:1,
+				num:4,
+				volunteerId:''
+			},
+			activityDialogVisible:false,
+			activityList:[{
+			    "id": 0,
+			    "name": "蓝天志愿者", 
+			    "status":"0",  
+			    "teamName":"蓝天志愿者团队",  
+			    "startDate":"2017-10-25",  
+			    "endDate":"2017-10-25" 
+			},{
+			    "id": 0,
+			    "name": "蓝天志愿者",  
+			    "status":"0",  
+			    "teamName":"蓝天志愿者团队",  
+			    "startDate":"2017-10-25",  
+			    "endDate":"2017-10-25"  
+			}]
+		}
+	},
+	methods:{
+		// ------ ajax请求 --------
+		// 获取志愿者列表
+		ajaxGetVolunteer(){
+			const t = this;
+			t.$http({
+				method:'post',
+				url:'/volunteer/ajax-get-all-volunteers',
+				body:t.pageParam
+			}).then(res => {
+				const result = res.data;
+				if(!result.status){
+					return t.$message({
+						message:result.message,
+						type:'error'
+					})
+				}
+				t.volunteerLen = result.data.maxPage * result.data.page;
+				t.volunteerList = result.data.list;
+			})
+		},
+		// 注销志愿者
+		ajaxDelete(){
+			const t = this;
+			t.activeVolunteer['status']=0;
+			t.$http({
+				method:'post',
+				url:'/volunteer/ajax-log-out-volunteers',
+				body:t.activeVolunteer
+			}).then(res => {
+				const result = res.data;
+				if(!result.status){
+					return t.$message({
+						message:result.message,
+						type:'error'
+					})
+				}
+
+				t.ajaxGetVolunteer();
+				t.deleteDialogVisible = false;
+			})
+		},
+		// 获取志愿活动列表
+		ajaxGetActive(){
+			const t = this;
+			t.activityPageParam['volunteerId'] = t.activeVolunteer['id'];
+			t.$http({
+				method:'post',
+				url:'/volunteer/ajax-get-volunteer-activity',
+				body:t.activityPageParam
+			}).then(res => {
+				const result = res.data;
+				if(!result.status){
+					return t.$message({
+						message:result.message,
+						type:'error'
+					})
+				}
+				t.activityList = result.data.list;
+			})
+		},
+		// ------ 其他操作 --------
+		// 换页
+		handlerPage(page){
+			const t = this;
+			t.pageParam['page'] = page;
+			t.ajaxGetVolunteer();
+		},
+		// tab切换
+		handlerTab(){
+			const t = this;
+			t.currentPage = 1;
+			t.pageParam['type'] = Number(t.activeTabName);
+			t.pageParam['page'] = 1;
+			t.ajaxGetVolunteer();
+		},
+		// 注销志愿者
+		handlerDelete(scope){
+			const t = this;
+			t.activeVolunteer = scope.row;
+			t.deleteDialogVisible = true;
+		},
+		// 志愿活动
+		handlerActivity(row, event, column){
+			const t = this;
+			t.activeVolunteer = row;
+			t.activityDialogVisible = true;
+			t.ajaxGetActive();
+		},
+		// 志愿活动换页
+		handlerActivityPage(page){
+			const t = this;
+			t.activityPageParam['page'] = page;
+			t.ajaxGetActive();
 		}
 	},
 	components:{
@@ -20,4 +225,27 @@ export default{
 </script>
 <style lang="scss">
 @import '../../../../src/assets/scss/baseParams.scss';
+.volunteer-container{
+	height:100%;
+	.volunteer-content{
+		padding:20px;
+		height:82%;
+
+		.table-box{
+			height:83%;
+			cursor: pointer;
+			.operate{
+				padding:10px;
+				&:hover{
+					color:$btnActive;
+				}
+			}
+		}
+	}
+	.el-pagination{
+		text-align: right;
+		margin-top: 15px;
+	}
+
+}
 </style>
